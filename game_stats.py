6a5a0ca1c435b9c28ef2eb0d112f4bf6954b8f29
Jordan Callahan <32796129+jordancallahan@users.noreply.github.com>
2022-06-02 20:03:36 -0400

    initial commit

---
+++ game_stats.py
@@ -0,0 +1,21 @@
+class GameStats:
+    """Track stats for Alien Invasion."""
+
+    def __init__(self, ai_game) -> None:
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False
+
+        # High score (should never be reset).
+        with open("high_score.txt") as f:
+            hi_score = f.read()
+        self.high_score = int(hi_score)
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
